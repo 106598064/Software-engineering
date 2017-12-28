083a19d98ef4aa72aa4aa4ea4418b72b7c8f4625
賴偉程 <arnold2841@gmail.com>
2017-12-29 01:01:44 +0800

    hw4

---
+++ src/gradeletter/TestGradeLetter.java
@@ -0,0 +1,81 @@
+package gradeletter;
+import junit.framework.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGradeLetter extends TestCase{
+	
+	public int Input;
+	public char Result;
+	public char letterGrade;
+	
+	@Test
+	public void test_1() {
+		Input = 14 ;
+		Result = 'F' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	
+	public void test_2() {
+		Input = -3 ;
+		Result = 'X' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	public void test_3() {
+		Input = 0 ;
+		Result = 'F' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	public void test_4() {
+		Input = 66 ;
+		Result = 'D' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	
+	public void test_5() {
+		Input = 87 ;
+		Result = 'B' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	public void test_6() {
+		Input = 99 ;
+		Result = 'A' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	public void test_7() {
+		Input = 120 ;
+		Result = 'X' ;
+		assertEquals(Result,letterGrade(Input));
+	}
+	
+	
+	
+	
+	
+	private static char letterGrade(int score) {
+		char grade ;
+		if(score<0 || score>100) {
+			grade='X';
+		}else if(score>=90 &&score<=100){
+			grade='A';
+		}else if(score>=80 &&score<90) {
+			grade='B';
+		}else if(score>=70 &&score<80) {
+			grade='C';
+		}else if(score>=60 &&score<70) {
+			grade='D';
+		}else {
+			grade='F';
+		}
+		return grade;
+	}
+
+
+}
